e6eff8a7ea607b6e7a7b42de4c9dfcbe10133def
mattis <mattiszydek@gmx.de>
2024-02-25 12:27:32 +0100

    add sum function

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+
+    int firstnumber = 5;
+    int secondnumber = 9;
+    int result = Integer.sum(firstnumber, secondnumber);
+    System.out.println(result);
   }
 }
